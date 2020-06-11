6adeaa0a099d5d68e1a4ee071932f61607d3e295
lixchen <lixiong.chen@capgemini.com>
2020-06-11 17:13:22 +0200

    module2

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,14 +4,19 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
+    }
+
+    private static int add(int[] numbers) {
+        var sum = 0;
+        for (int i : numbers)
+            sum += i;
+        return sum;
     }
 }
